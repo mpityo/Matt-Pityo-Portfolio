2ecf6a0179ace5321fc9c8030b5a1965eaa58b87
mpityo <mpityo@gmail.com>
2022-12-28 20:53:32 -0500

    three js and animated avatar

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Model } from "../Model/Model";
 
 function About() {
   return (
     <section className="mx-4 about">
       <h1 className="text-align">Matt Pityo</h1>
       <div className="d-flex flex-wrap about-container mt-1">
-        <div className="flex-row bio-img-container">
-          <img
+        {/* <div className="flex-row bio-img-container"> */}
+        {/* <img
             className="img-fluid rounded bio-img"
-            src="./assets/bio-img.jpeg"
+            src={process.env.PUBLIC_URL + "/assets/bio-img.jpeg"}
             alt="Matt looking up towards the sky on the beach"
-          ></img>
-        </div>
+          ></img> */}
+        <Canvas
+          camera={{ position: [2, 0, 12.25], fov: 10 }}
+          style={{
+            backgroundColor: "transparent",
+            width: "30vw",
+            height: "60vh",
+            maxWidth: "350px",
+          }}
+        >
+          <ambientLight intensity={1.2} />
+          <ambientLight intensity={0.1} />
+          <directionalLight intensity={0.4} />
+          <Suspense fallback={null}>
+            <Model position={[0.025, -0.91, 0]}/>
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+        {/* </div> */}
         <div className="col-md-8 flex-row bio m-auto">
           <p>
             I am a full stack developer looking for opportunities to connect and
